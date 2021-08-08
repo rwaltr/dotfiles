3c6fbc91beec204b764be9dbb69d82ebe4de4e28
Ryan Walter <rwalt@pm.me>
2021-08-08 13:31:31 -0500

    script update
    Signed-off-by: Ryan Walter <rwalt@pm.me>

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,16 +1,17 @@
-{{- if eq .chezmoi.os "linux" }}
+{{- if eq .chezmoi.os "linux" -}}
 #!/bin/bash
-{{- if eq .chezmoi.osRelease.name "Fedora" }}
-echo "Installing packages"
+{{ if eq .chezmoi.osRelease.name "Fedora" -}}
+echo "Installing script packages"
 sudo dnf update -y
 sudo dnf install toilet git wget curl -y
 toilet --metal "Installing RPM Fusion"
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 toilet --metal "RPMFUSION UPDATES"
 sudo dnf groupupdate core -y
 toilet "Installing Basic Tools" --metal
 sudo dnf install -y neovim lsd jq 
-toilet "Updating Vim"
-nvim --headless -c PlugInstall
+toilet "Updating Vim" --metal
+nvim -c PlugInstall +qall
+toilet "Vim updated" --metal
 {{- end }}
 {{- end }}
