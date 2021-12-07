d4659847ae04ff64feb61da774b3a4998d4cfe31
rwaltr <rwalt@pm.me>
2021-12-06 21:32:14 -0600

    Plugins added as needed
    Signed-off-by: rwaltr <rwalt@pm.me>

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -93,7 +93,9 @@ use { 'tami5/lspsaga.nvim' }
      'hrsh7th/cmp-buffer',
      'hrsh7th/cmp-path',
      'hrsh7th/cmp-cmdline',
-     'hrsh7th/nvim-cmp'
+     'hrsh7th/nvim-cmp',
+     'hrsh7th/cmp-vsnip',
+     'hrsh7th/vim-vsnip'
    }
    -- TELESCOPIC
 use {
