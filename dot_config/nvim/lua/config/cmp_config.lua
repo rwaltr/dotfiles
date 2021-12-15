0151200f77eee7130cb510dc5e37e707febfaf74
Ryan Walter <rwalt@pm.me>
2021-12-15 11:29:38 -0600

    all buffers
    Signed-off-by: Ryan Walter <rwalt@pm.me>

--- dot_config/nvim/lua/config/cmp_config.lua
+++ dot_config/nvim/lua/config/cmp_config.lua
@@ -30,7 +30,14 @@
       -- { name = 'ultisnips' }, -- For ultisnips users.
       -- { name = 'snippy' }, -- For snippy users.
     }, {
-      { name = 'buffer' },
+			{
+				name = "buffer",
+				option = {
+					get_bufnrs = function()
+						return vim.api.nvim_list_bufs()
+					end,
+				},
+    },
     })
   })
 
