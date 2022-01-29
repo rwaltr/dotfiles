18ae19016ba8a54af575d64f6adbe24f2c0ab4f6
rwaltr <rwalt@pm.me>
2022-01-28 18:23:30 -0600

    bassyfish
    Signed-off-by: rwaltr <rwalt@pm.me>

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -1,3 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+bass eval (gnome-keyring-daemon --start)
+export SSH_AUTH_SOCK
+fish_add_path ~/.local/bin
+fish_add_path ~/bin
 end
