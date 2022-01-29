1b0a200bb548125648b2cf965242c83fb1424a5f
Ryan Walter <rwalt@pm.me>
2022-01-28 18:55:31 -0600

    keys r cool
    Signed-off-by: Ryan Walter <rwalt@pm.me>

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -1,7 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+  if type -q gnome-keyring-daemon 
 bass eval (gnome-keyring-daemon --start)
 export SSH_AUTH_SOCK
+  end
 fish_add_path ~/.local/bin
 fish_add_path ~/bin
 end
