d264bd8b6e3f3708a650166ddcb2ef95c4d57adf
Ryan Walter <rwalt@pm.me>
2022-02-11 01:15:54 -0600

    aqua?
    Signed-off-by: Ryan Walter <rwalt@pm.me>

---
+++ home/dot_config/private_fish/conf.d/aqua.fish
@@ -0,0 +1,4 @@
+set -gx AQUA_CONFIG $HOME/.config/aqua/aqua.yaml
+set -gx AQUA_GLOBAL_CONFIG $HOME/.config/aqua/aqua.yaml
+set -gx AQUA_ROOT_DIR $HOME/.local/share/aqua
+fish_add_path $AQUA_ROOT_DIR/bin
