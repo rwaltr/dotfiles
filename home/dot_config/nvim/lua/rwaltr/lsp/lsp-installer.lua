22cb74c2b743f29ddfd073c8228fcb043ee1c582
rwaltr <rwalt@pm.me>
2022-04-06 15:18:45 -0500

    feat: Yamlls configuration and Schemastore imports
    Signed-off-by: rwaltr <rwalt@pm.me>

--- home/dot_config/nvim/lua/rwaltr/lsp/lsp-installer.lua
+++ home/dot_config/nvim/lua/rwaltr/lsp/lsp-installer.lua
@@ -21,6 +21,11 @@ lsp_installer.on_server_ready(function(server)
 		opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
 	end
 
+  if server.name == "yamlls" then
+    local yamlls_opts = require("rwaltr.lsp.settings.yamlls")
+    opts = vim.tbl_deep_extend("force", yamlls_opts, opts)
+  end
+
 	-- This setup() function is exactly the same as lspconfig's setup function.
 	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 	server:setup(opts)
