7468ca249a36c214f0b386422508edd7219f09d3
rwaltr <rwalt@pm.me>
2022-04-27 14:40:18 -0500

    fix!: Removed lazygit
    Signed-off-by: rwaltr <rwalt@pm.me>

--- home/dot_config/nvim/lua/rwaltr/whichkey.lua
+++ home/dot_config/nvim/lua/rwaltr/whichkey.lua
@@ -108,7 +108,6 @@ local mappings = {
 
 	g = {
 		name = "Git",
-		g = { "<cmd>lua _LAZYGIT_TOGGLE()<CR>", "Lazygit" },
 		j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
 		k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
 		l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
