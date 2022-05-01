46db42be2155b633d4c3d501c09009e2951d2c16
Ryan Walter <rwalt@pm.me>
2022-05-01 01:37:04 -0500

    removed user

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -65,12 +65,6 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.rwaltr = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
-    password = "dank";
-  };
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
