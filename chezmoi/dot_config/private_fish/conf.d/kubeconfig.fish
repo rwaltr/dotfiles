4a2df9671c4647a5625542468a31c6514f229b5a
Ryan Walter <ryan.walter@sectigo.com>
2022-05-09 10:42:46 -0500

    kubeconfig function
    Signed-off-by: Ryan Walter <ryan.walter@sectigo.com>

--- chezmoi/dot_config/private_fish/conf.d/kubeconfig.fish
+++ chezmoi/dot_config/private_fish/conf.d/kubeconfig.fish
@@ -1,18 +1,1 @@
-# default kubeconfig
-set FILES "$HOME/.kube/config"
-
-# create clusters directory if its not created
-set CLUSTERS_DIR "$HOME/.kube/clusters"
-mkdir -p "$CLUSTERS_DIR"
-test CLUSTERS_DIR
-
-for cluster in (find $CLUSTERS_DIR -type f -name "*.yml")
-    set FILES "$cluster:$FILES"
-end
-
-for cluster in (find $CLUSTERS_DIR -type f -name "*.yaml")
-    set FILES "$cluster:$FILES"
-end
-
-
-set -gx KUBECONFIG $FILES
+set-kubeconfig
