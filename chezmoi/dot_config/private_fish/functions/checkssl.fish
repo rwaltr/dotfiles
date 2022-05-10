de81d0c3b807f46189f1fd379fca7cfb1593fcb5
Ryan Walter <ryan.walter@sectigo.com>
2022-05-10 13:51:38 -0500

    check
    Signed-off-by: Ryan Walter <ryan.walter@sectigo.com>

---
+++ chezmoi/dot_config/private_fish/functions/checkssl.fish
@@ -0,0 +1,7 @@
+function checkssl
+	if count $argv > /dev/null
+		echo | openssl s_client -servername {$argv} -connect {$argv}:443 2>/dev/null | openssl x509 -noout -dates
+	else
+		echo "Please supply a domain name."
+	end
+end
