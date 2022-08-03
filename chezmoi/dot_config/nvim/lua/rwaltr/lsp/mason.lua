5c279d41e565ef0c9bddba5c70f110ee24828852
Ryan Walter <rwalt@pm.me>
2022-08-02 22:15:45 -0500

    feat:Mason is working
    Signed-off-by: Ryan Walter <rwalt@pm.me>

--- chezmoi/dot_config/nvim/lua/rwaltr/lsp/mason.lua
+++ chezmoi/dot_config/nvim/lua/rwaltr/lsp/mason.lua
@@ -1,5 +1,9 @@
 -- Import mason and lspconfig
-local mason_lspconfig_ok, mason_lspconfig = pcall(require, "mason-lspconfig") 
+local status_ok, mason = pcall(require, "mason")
+if not status_ok then
+  return
+end
+local mason_lspconfig_ok, mason_lspconfig = pcall(require, "mason-lspconfig")
 if not mason_lspconfig_ok then
   return
 end
@@ -8,22 +12,77 @@ if not lspconfig_status_ok then
   return
 end
 
+-- Define Mason settings
+local servers = {
+  "terraformls",
+  "jsonls",
+  "yamlls",
+  "sumneko_lua",
+  "tflint",
+  "bashls"
+}
+
+local settings = {
+  ui = {
+    border = "rounded",
+  },
+  log_level = vim.log.levels.INFO,
+  max_concurrent_installers = 4,
+}
+
+mason.setup(settings)
+mason_lspconfig.setup {
+  ensure_installed = servers,
+  automatic_installation = true,
+}
+
 -- init ops 🤷
-local opts = {}
+local opts = {
+  on_attach = require("rwaltr.lsp.handlers").on_attach,
+  capabilities = require("rwaltr.lsp.handlers").capabilities,
+}
 
 -- Mason setup_handlers expects the first key to be the "default function"
 -- Then you can custiomize the handler using a key=value pair
 -- [yamlls] = function() print hello end
 mason_lspconfig.setup_handlers({
   -- Default Function
-  function (server_name)
-   opts = {
-      on_attach = require("rwaltr.lsp.handlers").on_attach, 
-      capabilities = require("rwaltr.lsp.handlers").capabilities,
-    }
-    lspconfig[server_name].setup{opts}
+  function(server_name)
+    lspconfig[server_name].setup({ opts })
   end,
-  ["jsonls"] = function ()
-    lspconfig.jsonls.setup{opts}
+  -- JsonLS settings
+  ["jsonls"] = function()
+    local jsonls_opts = require("rwaltr.lsp.settings.jsonls")
+    local extended_opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+    lspconfig.jsonls.setup({ extended_opts })
+  end,
+  -- Yamlls settings
+  ["yamlls"] = function()
+    local yamls_opts = require("rwaltr.lsp.settings.yamlls")
+    local extended_opts = vim.tbl_deep_extend("force", yamls_opts, opts)
+    lspconfig.yamlls.setup({ extended_opts })
+  end,
+  -- Lua Settings
+  ["sumneko_lua"] = function()
+    -- OLD
+    -- local lua_opts = require("rwaltr.lsp.settings.sumneko_lua")
+    -- local extended_opts = vim.tbl_deep_extend("force", lua_opts, opts)
+    -- lspconfig.sumneko_lua.setup { extended_opts }
+    -- /OLD
+
+    --#region Luadev
+    local l_status, lua_dev = pcall(require, "lua-dev")
+    if not l_status then
+      return
+    end
+
+    local luadev = lua_dev.setup({
+      lspconfig = {
+        on_attach = opts.on_attach,
+        capabilities = opts.capabilities,
+      },
+    })
+    --#endregion Luadev
+    lspconfig.sumneko_lua.setup(luadev)
   end,
 })
