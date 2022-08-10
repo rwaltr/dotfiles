994128e9ab83d8ea3b65e51ad49829a28126c9d2
Ryan Walter <ryan.walter@sectigo.com>
2022-08-10 09:14:36 -0500

    spaces
    Signed-off-by: Ryan Walter <ryan.walter@sectigo.com>

--- chezmoi/dot_config/nvim/lua/rwaltr/indentline.lua
+++ chezmoi/dot_config/nvim/lua/rwaltr/indentline.lua
@@ -53,13 +53,13 @@ vim.wo.colorcolumn = "99999"
 -- vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
 -- vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
 vim.opt.list = true
-vim.opt.listchars:append("space:⋅")
+-- vim.opt.listchars:append("space:⋅")
 -- vim.opt.listchars:append "space:"
 -- vim.opt.listchars:append("eol:↴")
 
 indent_blankline.setup({
-	-- show_end_of_line = false,
-	space_char_blankline = " ",
+	show_end_of_line = false,
+	-- space_char_blankline = '',
 	show_current_context = true,
 	show_current_context_start = true,
 	-- char_highlight_list = {
