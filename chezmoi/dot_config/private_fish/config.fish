b5ae22d2c0fef39b9e18ef0c6ec00c316d3dd52d
Ryan Walter <ryan.walter@sectigo.com>
2022-08-11 12:44:07 -0500

    fix cdpath
    Signed-off-by: Ryan Walter <ryan.walter@sectigo.com>

--- chezmoi/dot_config/private_fish/config.fish
+++ chezmoi/dot_config/private_fish/config.fish
@@ -5,7 +5,7 @@ if status is-interactive
         set -Ux SSH_AGENT_PID $SSH_AGENT_PID
         set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
     end
-    set -gx CDPATH ".:~:~/src/rwaltr:~/src/"
+    set -gx CDPATH . ~ ~/src/rwaltr ~/src/
     set -gx HISTSIZE 10000
     fish_vi_key_bindings
     set -U fish_greeting
