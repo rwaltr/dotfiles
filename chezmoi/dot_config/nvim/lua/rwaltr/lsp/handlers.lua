4e6c8ff30250e01f659b103bee23d213fdd8e43f
Ryan Walter <ryan.walter@sectigo.com>
2022-08-12 14:42:42 -0500

    nvim
    Signed-off-by: Ryan Walter <ryan.walter@sectigo.com>

--- chezmoi/dot_config/nvim/lua/rwaltr/lsp/handlers.lua
+++ chezmoi/dot_config/nvim/lua/rwaltr/lsp/handlers.lua
@@ -60,7 +60,7 @@ end
 ---@param bufnr any
 local function lsp_keymaps(bufnr)
   local opts = { noremap = true, silent = true }
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>Telescope lsp_declarations<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
--- chezmoi/dot_config/nvim/lua/rwaltr/telescope.lua
+++ chezmoi/dot_config/nvim/lua/rwaltr/telescope.lua
@@ -81,6 +81,45 @@ telescope.setup({
     },
   },
   pickers = {
+
+    live_grep = {
+      theme = "dropdown",
+    },
+    grep_string = {
+      theme = "dropdown",
+    },
+    find_files = {
+      theme = "dropdown",
+      previewer = false,
+    },
+    buffers = {
+      theme = "dropdown",
+      previewer = false,
+      initial_mode = "normal",
+    },
+    planets = {
+      show_pluto = true,
+      show_moon = true,
+    },
+    colorscheme = {
+      -- enable_preview = true,
+    },
+    lsp_references = {
+      theme = "dropdown",
+      initial_mode = "normal",
+    },
+    lsp_definitions = {
+      theme = "dropdown",
+      initial_mode = "normal",
+    },
+    lsp_declarations = {
+      theme = "dropdown",
+      initial_mode = "normal",
+    },
+    lsp_implementations = {
+      theme = "dropdown",
+      initial_mode = "normal",
+    },
     -- Default configuration for builtin pickers goes here:
     -- picker_name = {
     --   picker_config_key = value,
