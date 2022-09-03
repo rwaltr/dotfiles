823b77f341b5d72d675aacdcf4ebb7585f0d8a12
Ryan Walter <rwalt@pm.me>
2022-09-03 14:42:47 -0500

    stuff

--- chezmoi/dot_config/nvim/lua/rwaltr/lsp/null-ls.lua
+++ chezmoi/dot_config/nvim/lua/rwaltr/lsp/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-	return
+  return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -10,24 +10,25 @@ local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 local hover = null_ls.builtins.hover
 null_ls.setup({
-	debug = false,
-	on_attach = require("rwaltr.lsp.handlers").on_attach,
-	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--jsx-single-quote" } }),
-		-- formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.stylua,
-		formatting.shfmt,
-		diagnostics.codespell,
-		formatting.codespell,
-		diagnostics.shellcheck,
-		diagnostics.yamllint,
-		diagnostics.proselint,
-		code_actions.shellcheck,
-		code_actions.gitrebase,
-		code_actions.proselint,
-		formatting.fish_indent,
-		-- hover.dictionary,
-		-- diagnostics.flake8
-		code_actions.gitsigns,
-	},
+  debug = false,
+  on_attach = require("rwaltr.lsp.handlers").on_attach,
+  sources = {
+    formatting.prettier.with({ extra_args = { "--no-semi", "--jsx-single-quote" } }),
+    -- formatting.black.with({ extra_args = { "--fast" } }),
+    formatting.stylua,
+    formatting.shfmt,
+    diagnostics.codespell,
+    formatting.codespell,
+    diagnostics.shellcheck,
+    diagnostics.yamllint,
+    diagnostics.proselint,
+    code_actions.shellcheck,
+    code_actions.gitrebase,
+    code_actions.proselint,
+    formatting.fish_indent,
+    diagnostics.fish,
+    -- hover.dictionary,
+    -- diagnostics.flake8
+    code_actions.gitsigns,
+  },
 })
