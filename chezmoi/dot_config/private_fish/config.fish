1589bc3e7aff779ddf62b42541e346020c1480f2
Ryan Walter <rwalt@pm.me>
2022-09-03 14:59:16 -0500

    fixes
    Signed-off-by: Ryan Walter <rwalt@pm.me>

--- chezmoi/dot_config/private_fish/config.fish
+++ chezmoi/dot_config/private_fish/config.fish
@@ -1,10 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    if type -q gnome-keyring-daemon
-        bass eval (gnome-keyring-daemon)
-        set -Ux SSH_AGENT_PID $SSH_AGENT_PID
-        set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-    end
+    bass eval (gnome-keyring-daemon)
+    set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
     set -gx HISTSIZE 10000
     fish_vi_key_bindings
     set -U fish_greeting
