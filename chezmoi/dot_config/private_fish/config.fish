7fd70d16f47192b3231b2e5e1d27503cf1572e9c
Ryan Walter <ryan.walter@sectigo.com>
2022-09-07 11:24:09 -0500

    plswrl
    Signed-off-by: Ryan Walter <ryan.walter@sectigo.com>

--- chezmoi/dot_config/private_fish/config.fish
+++ chezmoi/dot_config/private_fish/config.fish
@@ -1,8 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    bass eval (gnome-keyring-daemon)
-    set -Ux SSH_AGENT_PID $SSH_AGENT_PID
-    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+    # bass eval (gnome-keyring-daemon)
+    # set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+    # set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
     set -gx HISTSIZE 10000
     fish_vi_key_bindings
     set -U fish_greeting
