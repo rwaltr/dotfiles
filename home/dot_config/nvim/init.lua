866da11a3cc9960bb8bab6dfb236f86cca63048b
Ryan Walter <rwalt@pm.me>
2023-01-03 22:35:54 -0600

    starting to get lazy
    Signed-off-by: Ryan Walter <rwalt@pm.me>

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -1,6 +1,2 @@
 require("rwaltr.core")
--- require("rwaltr.config.lazy")
--- enable staticy generated color theme
--- Refactor lsp
-require("rwaltr.plugins.lsp")
--- Migrate plugins.lua to be part of either init.lua, rwaltr.plugins, or use something like folke's plugin rendering
+require("rwaltr.config.lazy")
