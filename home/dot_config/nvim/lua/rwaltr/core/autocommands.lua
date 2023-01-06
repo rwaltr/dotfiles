0cd630f90832ec61acbffb1e9b0aa9cf33e85f19
Ryan Walter <rwalt@pm.me>
2023-01-06 03:24:51 -0600

    I wish I was smart enough to do this sooner
    Signed-off-by: Ryan Walter <rwalt@pm.me>

--- home/dot_config/nvim/lua/rwaltr/core/autocommands.lua
+++ home/dot_config/nvim/lua/rwaltr/core/autocommands.lua
@@ -1,68 +1,81 @@
 --#region git
 local gitgroup = vim.api.nvim_create_augroup("_git", { clear = true })
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "gitcommit",
-  callback = function()
-    vim.api.nvim_set_option_value("wrap", true, { scope = "local" })
-    vim.api.nvim_set_option_value("spell", true, { scope = "local" })
-  end,
-  group = gitgroup
+	pattern = "gitcommit",
+	callback = function()
+		vim.api.nvim_set_option_value("wrap", true, { scope = "local" })
+		vim.api.nvim_set_option_value("spell", true, { scope = "local" })
+	end,
+	group = gitgroup,
 })
 --#endregion git
 
 --#region chezmoi
 local chezmoigroup = vim.api.nvim_create_augroup("_chezmoi", { clear = true })
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  pattern = "~/.local/share/chezmoi/chezmoi/*",
-  callback = function()
-    -- TODO: Chezmoi apply
-  end,
-  group = chezmoigroup,
+	pattern = "**/.local/share/chezmoi/home/dot_config/nvim/**",
+	callback = function()
+		local job = require("plenary.job")
+		job:new({
+      -- TODO: a better notification
+			command = "chezmoi",
+			args = { "apply", "--source-path", vim.api.nvim_buf_get_name(0) },
+			on_start = function()
+				vim.notify("Writing for" .. vim.api.nvim_buf_get_name(0))
+			end,
+			on_exit = function()
+				vim.notify("Chezmoi Complete")
+			end,
+		}):start()
+	end,
+	group = chezmoigroup,
 })
 --#endregion chezmoi
-
+-- TODO: This should be better handled by ftdetect folders
 --#region svn
 local svngroup = vim.api.nvim_create_augroup("_svn", { clear = true })
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "svn",
-  callback = function()
-    vim.api.nvim_set_option_value("wrap", true, { scope = "local" })
-    vim.api.nvim_set_option_value("spell", true, { scope = "local" })
-  end,
-  group = svngroup,
+	pattern = "svn",
+	callback = function()
+		vim.api.nvim_set_option_value("wrap", true, { scope = "local" })
+		vim.api.nvim_set_option_value("spell", true, { scope = "local" })
+	end,
+	group = svngroup,
 })
 --#endregion svn
 
 --#region TextYankin
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
-  callback = function() vim.highlight.on_yank({ higroup = 'Visual', timeout = 200 }) end,
+	callback = function()
+		vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
+	end,
 })
 --#endregion TextYankin
 
 --#region Yaml
-local yamlgroup = vim.api.nvim_create_augroup('_yaml', { clear = true })
+local yamlgroup = vim.api.nvim_create_augroup("_yaml", { clear = true })
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "yaml",
-  group = yamlgroup,
-  callback = function()
-    vim.api.nvim_set_option_value("ts", 2, { scope = "local" })
-    vim.api.nvim_set_option_value("sts", 2, { scope = "local" })
-    vim.api.nvim_set_option_value("sw", 2, { scope = "local" })
-    vim.api.nvim_set_option_value("expandtab", true, { scope = "local" })
-  end
+	pattern = "yaml",
+	group = yamlgroup,
+	callback = function()
+		vim.api.nvim_set_option_value("ts", 2, { scope = "local" })
+		vim.api.nvim_set_option_value("sts", 2, { scope = "local" })
+		vim.api.nvim_set_option_value("sw", 2, { scope = "local" })
+		vim.api.nvim_set_option_value("expandtab", true, { scope = "local" })
+	end,
 })
 --#endregion Yaml
 
 --#region markdown
 local markdowngroup = vim.api.nvim_create_augroup("_markdown", { clear = true })
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = "markdown",
-  callback = function()
-    vim.api.nvim_set_option_value("wrap", true, { scope = "local" })
-    vim.api.nvim_set_option_value("spell", true, { scope = "local" })
-    -- changes ... to the elispis character to make proselint happy, but only in markdown files
-    vim.cmd("iab ... …")
-  end,
-  group = markdowngroup,
+	pattern = "markdown",
+	callback = function()
+		vim.api.nvim_set_option_value("wrap", true, { scope = "local" })
+		vim.api.nvim_set_option_value("spell", true, { scope = "local" })
+		-- changes ... to the elispis character to make proselint happy, but only in markdown files
+		vim.cmd("iab ... …")
+	end,
+	group = markdowngroup,
 })
 --#endregion svn
