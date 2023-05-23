3cb72130f2ce9b31ce176c7df735773fc6371064
Ryan Walter <rwalt@pm.me>
2023-05-23 18:02:56 -0500

    shrunk .bashrc
    Signed-off-by: Ryan Walter <rwalt@pm.me>

---
+++ home/dot_config/bash.d/0.opts.sh
@@ -0,0 +1,21 @@
+shopt -s autocd # change to named directory
+shopt -s cdspell # autocorrects cd misspellings
+shopt -s cmdhist # save multi-line commands in history as single line
+shopt -s dotglob
+shopt -s histappend # do not overwrite history
+shopt -s expand_aliases # expand aliases
+shopt -s checkwinsize # checks term size when bash regains control
+bind "set completion-ignore-case on"
+# Treat hyphens and underscores as equivalent
+bind "set completion-map-case on"
+# Display matches for ambiguous patterns at first tab press
+bind "set show-all-if-ambiguous on"
+# Immediately add a trailing slash when autocompleting symlinks to directories
+bind "set mark-symlinked-directories on"
+
+# Custom bookmarks
+shopt -s cdable_vars
+# export dotfiles="$HOME/dotfiles"
+# export projects="$HOME/projects"
+# export documents="$HOME/Documents"
+# export dropbox="$HOME/Dropbox"
