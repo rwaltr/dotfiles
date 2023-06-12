42dd8cec6ef0a57b83030f2ded1f65e0ecebc126
Ryan Walter <ryan.walter@sectigo.com>
2023-06-12 11:18:06 -0500

    zen
    Signed-off-by: Ryan Walter <ryan.walter@sectigo.com>

--- home/dot_config/nvim/lua/rwaltr/plugins/ui.lua
+++ home/dot_config/nvim/lua/rwaltr/plugins/ui.lua
@@ -215,9 +215,9 @@ return {
     "folke/zen-mode.nvim",
     cmd = "ZenMode",
     opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
+      window = {
+        width = 0.75,
+      },
     },
   },
   { "shortcuts/no-neck-pain.nvim",  version = "*", cmd = "NoNeckPain" },
