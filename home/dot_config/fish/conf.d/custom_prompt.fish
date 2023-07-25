c42dd7611dcb2d81e28503de019176a5d29fa73e
Ryan Walter <rwalt@pm.me>
2023-07-25 06:38:56 -0500

    Starship tweaks
    Signed-off-by: Ryan Walter <rwalt@pm.me>

--- home/dot_config/fish/conf.d/custom_prompt.fish
+++
@@ -1,5 +0,0 @@
-if status is-interactive
-    if type -q starship
-        source (starship init fish --print-full-init | psub)
-    end
-end
---
+++ home/dot_config/fish/conf.d/starship.fish
@@ -0,0 +1,13 @@
+function starship_transient_prompt_func
+  echo "-"(starship module character)
+end
+function starship_transient_rprompt_func
+   starship module time
+end
+
+if status is-interactive
+ if type -q starship
+  starship init fish | source
+  enable_transience
+  end
+end
