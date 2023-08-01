c911f5b89cbad6356edaf9b5cdb583cb6c404a8a
Ryan Walter <rwalt@pm.me>
2023-07-31 21:37:58 -0500

    fix(Neorg): Version lock

--- home/dot_config/nvim/lua/rwaltr/plugins/notetaking.lua
+++ home/dot_config/nvim/lua/rwaltr/plugins/notetaking.lua
@@ -7,10 +7,13 @@ return {
     "nvim-neorg/neorg",
     build = ":Neorg sync-parsers",
     cmd = "Neorg",
+    -- HACK Remove when https://github.com/nvim-neorg/neorg/issues/1002
+    commit = "e76f0cb6b3ae5e990052343ebb73a5c8d8cac783",
     ft = "norg",
     dependencies = {
       { "nvim-lua/plenary.nvim" },
-      { "nvim-neorg/neorg-telescope" },
+      { "nvim-neorg/neorg-telescope",
+        commit = "d24f445c912451ddbf17cbe8da36561b51b10d39", },
     },
     opts = {
       load = {
@@ -27,7 +30,6 @@ return {
           config = {
             workspaces = {
               notebook = "~/Documents/Notebook",
-              work = "~/Documents/Notebook/work",
             },
             default_workspace = "notebook",
           },
