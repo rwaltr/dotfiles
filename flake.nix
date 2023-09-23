4adb07d5cd4a0d41b251bb50a8be307648e7d9e0
Ryan Walter <rwalt@pm.me>
2023-09-23 15:06:34 -0500

    correct flake nonsense
    Signed-off-by: Ryan Walter <rwalt@pm.me>

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
       monolith = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
         # > Our main nixos configuration file <
-        modules = [./nixos/configuration.nix];
+        modules = [./nixox/nixos/configuration.nix];
       };
     };
 
@@ -44,7 +44,7 @@
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
         # > Our main home-manager configuration file <
-        modules = [./home-manager/home.nix];
+        modules = [./nixos/home-manager/home.nix];
       };
     };
   };
