56d9eb197946908c88f1655c2d4c647594852991
Ryan Walter <rwalt@pm.me>
2023-09-24 18:01:05 -0500

    move nix location to somewhere reasonable
    Signed-off-by: Ryan Walter <rwalt@pm.me>

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
       monolith = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
         # > Our main nixos configuration file <
-        modules = [./nixos/nixos/configuration.nix];
+        modules = [./nix/nixos/configuration.nix];
       };
     };
 
@@ -44,7 +44,7 @@
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
         # > Our main home-manager configuration file <
-        modules = [./nixos/home-manager/home.nix];
+        modules = [./nix/home-manager/home.nix];
       };
     };
   };
