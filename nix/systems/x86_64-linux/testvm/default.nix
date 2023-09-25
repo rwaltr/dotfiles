c0c1be7c26c6b3e3ec5897957d0d36bb6fe95bda
Ryan Walter <rwalt@pm.me>
2023-09-24 20:54:59 -0500

    oh

--- nix/systems/x86_64-linux/testvm/default.nix
+++ nix/systems/x86_64-linux/testvm/default.nix
@@ -35,7 +35,6 @@
   boot.loader.grub.device = "/dev/vda";
   boot.loader.grub.useOSProber = true;
 
-  networking.hostName = "test-vm"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
