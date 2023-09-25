8c0984a1411b8da4d851a882cb8cd6e8e6f3bc78
Ryan Walter <rwalt@pm.me>
2023-09-24 20:24:23 -0500

    oh lord

--- flake.nix
+++ flake.nix
@@ -15,37 +15,59 @@
     # Shameless plug: looking for a way to nixify your themes and make
     # everything match nicely? Try nix-colors!
     # nix-colors.url = "github:misterio77/nix-colors";
+
+     snowfall-lib = {
+        url = "github:snowfallorg/lib";
+        inputs.nixpkgs.follows = "nixpkgs";
+     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    ...
-  } @ inputs: let
-    inherit (self) outputs;
-  in {
-    # NixOS configuration entrypoint
-    # Available through 'nixos-rebuild --flake .#your-hostname'
-    nixosConfigurations = {
-      # FIXME replace with your hostname
-      testvm = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        # > Our main nixos configuration file <
-        modules = [./nix/nixos/configuration.nix];
-      };
-    };
 
-    # Standalone home-manager configuration entrypoint
-    # Available through 'home-manager --flake .#your-username@your-hostname'
-    homeConfigurations = {
-      # FIXME replace with your username@hostname
-      "rwaltr@monolith" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = {inherit inputs outputs;};
-        # > Our main home-manager configuration file <
-        modules = [./nix/home-manager/home.nix];
+outputs = inputs:
+inputs.snowfall-lib.mkFlake {
+    inherit inputs;
+
+    src = ./.;
+
+    snowfall = {
+      root = ./nix;
+      namespace = "waltr"
+      meta = {
+          name = "Rwaltr's Flake"
+        };
       };
-    };
   };
+
+#   outputs = {
+#     self,
+#     nixpkgs,
+#     home-manager,
+#     ...
+#   } @ inputs: let
+#     inherit (self) outputs;
+#   in {
+#     # NixOS configuration entrypoint
+#     # Available through 'nixos-rebuild --flake .#your-hostname'
+#     nixosConfigurations = {
+#       # FIXME replace with your hostname
+#       testvm = nixpkgs.lib.nixosSystem {
+#         specialArgs = {inherit inputs outputs;};
+#         # > Our main nixos configuration file <
+#         modules = [./nix/nixos/configuration.nix];
+#       };
+#     };
+#
+#     # Standalone home-manager configuration entrypoint
+#     # Available through 'home-manager --flake .#your-username@your-hostname'
+#     homeConfigurations = {
+#       # FIXME replace with your username@hostname
+#       "rwaltr@monolith" = home-manager.lib.homeManagerConfiguration {
+#         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+#         extraSpecialArgs = {inherit inputs outputs;};
+#         # > Our main home-manager configuration file <
+#         modules = [./nix/home-manager/home.nix];
+#       };
+#     };
+#   };
+
 }
