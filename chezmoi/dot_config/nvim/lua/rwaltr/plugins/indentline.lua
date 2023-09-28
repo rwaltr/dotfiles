8145020b9933aca056303d1b64650ffcf67d7a92
Ryan Walter <rwalt@pm.me>
2023-09-28 00:10:39 -0500

    updates
    Signed-off-by: Ryan Walter <rwalt@pm.me>

--- chezmoi/dot_config/nvim/lua/rwaltr/plugins/indentline.lua
+++ chezmoi/dot_config/nvim/lua/rwaltr/plugins/indentline.lua
@@ -1,63 +1,27 @@
 local indentsymbol = "╎"
-return { {
-  "lukas-reineke/indent-blankline.nvim",
-  event = "BufReadPre",
-  config = function()
-    local status_ok, indent_blankline = pcall(require, "indent_blankline")
-    if not status_ok then
-      return
-    end
-
-    vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile", "alpha", "neo-tree", "mind" }
-    vim.g.indent_blankline_filetype_exclude = {
-      "help",
-      "startify",
-      "dashboard",
-      "packer",
-      "lazy",
-      "neogitstatus",
-      "NvimTree",
-      "Trouble",
-    }
-    vim.g.indentLine_enabled = 1
-    vim.g.indent_blankline_char = indentsymbol
-    vim.g.indent_blankline_show_trailing_blankline_indent = false
-    vim.g.indent_blankline_show_first_indent_level = true
-    vim.g.indent_blankline_use_treesitter = true
-    vim.g.indent_blankline_show_current_context = false
-    vim.g.indent_blankline_context_patterns = {
-      "class",
-      "return",
-      "function",
-      "method",
-      "^if",
-      "^while",
-      "jsx_element",
-      "^for",
-      "^object",
-      "^table",
-      "block",
-      "arguments",
-      "if_statement",
-      "else_clause",
-      "jsx_element",
-      "jsx_self_closing_element",
-      "try_statement",
-      "catch_clause",
-      "import_statement",
-      "operation_type",
-    }
-
-    -- vim.opt.listchars:append("space:⋅")
-    vim.opt.listchars:append("eol:↴")
-
-    indent_blankline.setup({
-      show_end_of_line = false,
+return {
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+      -- char = "▏",
+      char = indentsymbol,
+      filetype_exclude = {
+        "help",
+        "alpha",
+        "dashboard",
+        "neo-tree",
+        "Trouble",
+        "lazy",
+        "mason",
+        "notify",
+        "toggleterm",
+        "lazyterm",
+      },
+      show_trailing_blankline_indent = false,
       show_current_context = false,
-      show_current_context_start = true,
-    })
-  end
-},
+    },
+  },
   {
     "echasnovski/mini.indentscope",
     version = false, -- wait till new 0.7.0 release to put it back on semver
