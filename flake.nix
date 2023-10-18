cd22e603e0aaf23f1c9b0b55e32167accc2d720e
Ryan Walter <rwalt@pm.me>
2023-10-17 19:44:34 -0500

    snapshot

--- flake.nix
+++ flake.nix
@@ -11,24 +11,32 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # Hardware specials
-    # hardware.url = "github:nixos/nixos-hardware";
+    hardware.url = "github:nixos/nixos-hardware";
 
     # Generate System Images
     nixos-generators.url = "github:nix-community/nixos-generators";
     nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
 
+    # Disk managment
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Formatting
+    nix-formatter-pack.url = "github:Gerschtli/nix-formatter-pack";
+    nix-formatter-pack.inputs.nixpkgs.follows = "nixpkgs";
+
     # Talhelper
-    talhelper.url = "github:budimanjojo/talhelper";
-    talhelper.inputs.nixpkgs.follows = "unstable";
+    # talhelper.url = "github:budimanjojo/talhelper";
+    # talhelper.inputs.nixpkgs.follows = "unstable";
 
     # deploy-rs
     # deploy-rs.url = "github:serokell/deploy-rs";
     # deploy-rs.inputs.nixpkgs.follows = "unstable";
 
-    snowfall-lib = {
-      url = "github:snowfallorg/lib";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # snowfall-lib = {
+    #   url = "github:snowfallorg/lib";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
 
 
