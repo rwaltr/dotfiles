458431072694084d367433fc5c1009604b12c098
Ryan Walter <rwaltr@rwalt.pro>
2023-11-16 22:39:58 -0600

    some fixes

--- chezmoi/dot_config/nvim/lua/rwaltr/plugins/ui.lua
+++ chezmoi/dot_config/nvim/lua/rwaltr/plugins/ui.lua
@@ -309,16 +309,16 @@ return {
           ["cmp.entry.get_documentation"] = true,
         },
       },
-      cmdline = {
-        view = "cmdline",
-      },
+      -- cmdline = {
+      --   view = "cmdline",
+      -- },
       -- you can enable a preset for easier configuration
       presets = {
-        bottom_search = true, -- use a classic bottom cmdline for search
-        command_palette = false, -- position the cmdline and popupmenu together
+        bottom_search = false, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
         long_message_to_split = true, -- long messages will be sent to a split
         inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false, -- add a border to hover docs and signature help
+        lsp_doc_border = true, -- add a border to hover docs and signature help
       },
     },
   },
