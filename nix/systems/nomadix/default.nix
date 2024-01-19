5f5cded95787ad111e7102c2d98674f9a71f944b
Ryan Walter <rwalt@pm.me>
2024-01-18 18:49:05 -0600

    nomadix

--- nix/systems/nomadix/default.nix
+++ nix/systems/nomadix/default.nix
@@ -99,7 +99,7 @@
   services.openssh.enable = true;
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 22 ... ];
+  networking.firewall.allowedTCPPorts = [ 22 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
