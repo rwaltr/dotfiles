f07c1f787b25123f391aab2891885a0c8b35037c
Ryan Walter <rwalt@pm.me>
2024-01-18 19:58:24 -0600

    nomadix

--- nix/systems/nomadix/default.nix
+++ nix/systems/nomadix/default.nix
@@ -79,20 +79,18 @@
   };
 
   # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    neovim
-    wget
-    curl
-    git
-    chezmoi
-    restic
-    rclone
     age
-    kubectl
-    gh
+    chezmoi
     croc
+    curl
+    gh
+    git
+    neovim
+    rclone
+    restic
+    wget
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
