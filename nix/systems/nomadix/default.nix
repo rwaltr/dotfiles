fd012c702ac77573becc53d7f4b07663d23b51b4
Ryan Walter <rwalt@pm.me>
2024-01-18 19:08:43 -0600

    nomadix

--- nix/systems/nomadix/default.nix
+++ nix/systems/nomadix/default.nix
@@ -2,13 +2,14 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, hardware, ... }:
 
 {
   imports =
     [
       # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      hardware.nixosModules.framework-13-7040-amd
     ];
 
   # Use the systemd-boot EFI boot loader.
