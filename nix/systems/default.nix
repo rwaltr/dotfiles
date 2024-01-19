9bc2f873a31164a221d58f64352fa8c4b23b435c
Ryan Walter <rwalt@pm.me>
2024-01-18 18:48:04 -0600

    nomadix

--- nix/systems/default.nix
+++ nix/systems/default.nix
@@ -1,28 +1,30 @@
-{inputs, ...}: {
-  flake.nixosConfigurations = let
-    inherit (inputs.nixpkgs.lib) nixosSystem;
-  in {
-    iso = nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-        "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
-        ({...}: {
-          nix.settings.experimental-features = ["nix-command" "flakes"];
-        })
-      ];
+{ inputs, ... }: {
+  flake.nixosConfigurations =
+    let
+      inherit (inputs.nixpkgs.lib) nixosSystem;
+    in
+    {
+      iso = nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+          "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
+          ({ ... }: {
+            nix.settings.experimental-features = [ "nix-command" "flakes" ];
+          })
+        ];
+      };
+      testvm = nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./testvm
+        ];
+      };
+      nomadix = nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./nomadix
+        ];
+      };
     };
-    testvm = nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./testvm
-      ];
-    };
-    nomadix = nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./nomadix
-      ];
-    };
-  };
 }
