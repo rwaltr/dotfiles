792b041c764ccfa3ea570ce037baed0fced23af5
Ryan Walter <rwalt@pm.me>
2024-01-28 15:31:19 -0600

    nomadix

--- nix/systems/nomadix/default.nix
+++ nix/systems/nomadix/default.nix
@@ -63,6 +63,7 @@
   programs.hyprland.enable = true;
   programs.waybar.enable = true;
   programs.sway.enable = true;
+  programs.fish.enable = true;
 
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
