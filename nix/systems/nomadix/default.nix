17aa49ec331bd3428781adeebce671df2c9dbcdb
Ryan Walter <rwalt@pm.me>
2024-02-26 16:39:34 -0600

    power mgmt

--- nix/systems/nomadix/default.nix
+++ nix/systems/nomadix/default.nix
@@ -168,6 +168,31 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  # Battery Life Improvement
+  # https://github.com/TechsupportOnHold/Batterylife/blob/main/laptop.nix
+  # Better scheduling for CPU cycles - thanks System76!!!
+  services.system76-scheduler.settings.cfsProfiles.enable = true;
+
+  # Enable TLP (better than gnomes internal power manager)
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    };
+  };
+
+  # Disable GNOMEs power management
+  services.power-profiles-daemon.enable = false;
+
+  # Enable powertop
+  powerManagement.powertop.enable = true;
+
+  # Enable thermald (only necessary if on Intel CPUs)
+  services.thermald.enable = true;
+
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
