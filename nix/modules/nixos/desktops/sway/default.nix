34b7ffcdfbb41cf82b87995e831ef718d884f1d8
Ryan Walter <rwalt@pm.me>
2024-03-04 19:06:32 -0600

    module?

--- nix/modules/nixos/desktops/sway/default.nix
+++ nix/modules/nixos/desktops/sway/default.nix
@@ -3,12 +3,14 @@
   programs.sway.enable = true;
   services.pipewire.enable = true;
   services.pipewire.pulse.enable = true;
-  programs.waybar.enable = true;
+  # programs.waybar.enable = true;
 
+  # This is defaults for the desktop that assume no user config
   environment.systemPackages = with pkgs;[
-    wofi
+    # wofi
+    dmenu
     firefox-wayland
-    wezterm
-    networkmanagerapplet
+    # wezterm
+    # networkmanagerapplet
   ];
 }
