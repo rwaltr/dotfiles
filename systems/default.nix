8320f79d527cfe244cbd60ee0107927078539c5b
Ryan Walter <rwalt@pm.me>
2024-08-10 21:43:19 -0500

    stuffz

--- systems/default.nix
+++ systems/default.nix
@@ -6,6 +6,7 @@
         imports = [
           "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
           "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
+          self.nixosModules.common
           ./bootstrap.nix
         ];
       };
@@ -20,6 +21,7 @@
         nixpkgs.hostPlatform = "x86_64-linux";
         imports = [
           inputs.disko.nixosModules.default
+          self.nixosModules.common
           self.homeModules.common-linux
           ./testvm
         ];
