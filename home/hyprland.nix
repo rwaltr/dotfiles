17f2ae8d4c6e20ed60ab339712357ef94f017a2b
Ryan Walter <rwalt@pm.me>
2024-08-23 17:58:10 -0500

    hyprland

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -7,6 +7,12 @@
     xwayland.enable = true;
     systemd.enable = true;
     settings = {
+      bind = [
+        "$mod, Space, exec, wofi"
+        "$mod, Enter, exec, wezterm"
+        "$mod, Q, killactive"
+        "$mod, E, exit"
+      ];
 
       "$mod" = "SUPER";
 
