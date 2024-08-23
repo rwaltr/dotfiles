cd27110171502108844995cee0bc3b8e50e5127e
Ryan Walter <rwalt@pm.me>
2024-08-23 17:04:43 -0500

    home in home not homehome

--- home/nix.nix
+++ home/nix.nix
@@ -4,5 +4,5 @@
 
   # Nix configuration is managed globally by nix-darwin.
   # Prevent $HOME nix.conf from disrespecting it.
-  home.file."~/.config/nix/nix.conf".text = "";
+  home.file.".config/nix/nix.conf".text = "";
 }
