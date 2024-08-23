4210b31ff4a45bb53b3d739601c6c2f8cfb5ed89
Ryan Walter <rwalt@pm.me>
2024-08-23 17:30:38 -0500

    hyprland

--- home/default.nix
+++ home/default.nix
@@ -11,6 +11,7 @@
       common-linux = {
         imports = [
           self.homeModules.common
+          ./hyprland.nix
         ];
       };
       common-darwin = {
---
+++ home/hyprland.nix
@@ -0,0 +1,4 @@
+{ ... }: {
+  wayland.windowManager.hyprland.enable = true;
+  wayland.windowManager.hyprland.systemd.enable = true;
+}
