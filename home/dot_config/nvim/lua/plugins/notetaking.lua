c215506e8d5b5944518255d0333cf93eab67fac3
Ryan Walter <rwaltr@rwalt.pro>
2025-08-06 16:13:49 -0500

    yooooooo

--- home/dot_config/nvim/lua/plugins/notetaking.lua
+++ home/dot_config/nvim/lua/plugins/notetaking.lua
@@ -29,57 +29,6 @@ return {
     },
   },
   {
-    "nvim-neorg/neorg",
-    -- build = ":Neorg sync-parsers",
-    -- version = "v7.0.0",
-    build = false,
-    cmd = "Neorg",
-    ft = "norg",
-    keys = {
-      { "<leader>ni", "<cmd>Neorg index<cr>", desc = "Neorg Index" },
-      { "<leader>nj", "<cmd>Neorg journal<cr>", desc = "Neorg journal" },
-      { "<leader>nr", "<cmd>Neorg return<cr>", desc = "Return from Neorg" },
-    },
-    dependencies = {
-      { "nvim-lua/plenary.nvim" },
-      { "nvim-neorg/neorg-telescope" },
-      {
-        "which-key.nvim",
-        optional = true,
-        opts = {
-          defaults = {
-            ["<leader>n"] = { name = "Neorg" },
-          },
-        },
-      },
-    },
-    opts = {
-      load = {
-        ["core.defaults"] = {},
-        ["core.concealer"] = {
-          config = { icon_preset = "diamond" },
-        },
-        ["core.integrations.telescope"] = {},
-        ["core.completion"] = {
-          config = { engine = "nvim-cmp" },
-        },
-        ["core.integrations.nvim-cmp"] = {},
-        ["core.dirman"] = {
-          config = {
-            workspaces = {
-              notebook = "~/Documents/Notebook",
-            },
-            default_workspace = "notebook",
-          },
-        },
-        ["core.journal"] = {
-          config = { workspace = "notebook" },
-        },
-        ["core.summary"] = {},
-      },
-    },
-  },
-  {
     "jakewvincent/mkdnflow.nvim",
     enabled = true,
     config = true,
