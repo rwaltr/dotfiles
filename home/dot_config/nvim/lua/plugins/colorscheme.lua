580e788dcca4fde110784d8cf400ea1ec2ed1876
Ryan Walter <rwaltr@rwalt.pro>
2026-03-23 16:36:48 -0500

    configure catppuccin mocha for neovim with integrations
    - Pin flavour to mocha (matches wezterm, starship, tv)
    - Enable term_colors for consistent terminal inside nvim
    - Enable integrations: diffview, flash, gitsigns, mason, mini,
      native_lsp, noice, notify, snacks, telescope, treesitter,
      treesitter_context, which_key

--- home/dot_config/nvim/lua/plugins/colorscheme.lua
+++ home/dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,37 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha",
+      term_colors = true,
+      default_integrations = true,
+      integrations = {
+        diffview = true,
+        flash = true,
+        gitsigns = true,
+        mason = true,
+        mini = { enabled = true },
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "underline" },
+            hints = { "underline" },
+            warnings = { "underline" },
+            information = { "underline" },
+          },
+        },
+        noice = true,
+        notify = true,
+        snacks = true,
+        telescope = { enabled = true },
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
